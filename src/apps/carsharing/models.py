e1bab7916988be3583b7be0ca731039c9ed22326
Jqsm6 <jqsm6@yandex.ru>
2023-12-07 15:42:57 +0300

    feat: add car model

--- src/apps/carsharing/models.py
+++ src/apps/carsharing/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    """Rental car model."""
+
+    car_brand = models.CharField('Car brand', max_length=50)
+    car_model = models.CharField('Car model', max_length=50)
+    car_year = models.CharField('Car year', max_length=4)
+    status = models.CharField('Status', max_length=10, choices=[('Ready', 'Busy')])
+
+    def __str__(self):
+        return f"{self.car_brand} - {self.car_model}"
+
