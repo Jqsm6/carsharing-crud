56be08f695ba037b0c5a4a75b7e0c136df2dce10
Jqsm6 <jqsm6@yandex.ru>
2023-12-08 09:16:46 +0300

    feat: add new route

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -1,9 +1,9 @@
-from api.v1.carsharing import views as api_views
-from apps.carsharing import views as app_views
 from django.urls import path
+
+from api.v1.carsharing import views as api_views
 
 urlpatterns = [
     path("v1/health", api_views.health_check, name="health"),
-    path("v1/cars", app_views.CarCrudView.as_view(), name="cars"),
-    path("v1/offers", app_views.OfferCrudView.as_view(), name="offers"),
+    path("v1/cars", api_views.CarSharingView.as_view(), name="cars"),
+    path("v1/offers", api_views.OfferView.as_view(), name="offers")
 ]
